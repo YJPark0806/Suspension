f1aa2e46c6b47d5197c9f74e4bfa7a167f647f47
Youngju <parky21@my.erau.edu>
2025-08-05 16:58:14 +0900

    Lidar Spec change
    - Changed Lidar pos to avoid disturbance from car
    - Changed Lidar max scan value to 5m

--- utils/lidar.py
+++ utils/lidar.py
@@ -1,19 +1,19 @@
 import numpy as np
 import mujoco
 
-def get_lidar_scan(model, data, site_name, num_rays=30, max_dist=1.0):
+def get_lidar_scan(model, data, site_name, num_rays=30, max_dist=5.0):
     site_id = model.site(site_name).id
     origin = data.site_xpos[site_id].copy()
     rot_mat = data.site_xmat[site_id].reshape(3, 3)  # Local → World 회전행렬
 
     # 차량 기준 벡터
-    forward = np.array([1, 0, 0])
-    up = np.array([0, 0, -1])  # 아래쪽
+    forward = np.array([1, 0, 0])  # theta=0일 때는 차량 정면방향
+    up = np.array([0, 0, -1])  # theta=90일 때 차량 아래방향
     # 측정 각도: 
-    angles = np.radians(np.linspace(60, 120, num_rays))
+    angles = np.radians(np.linspace(30, 150, num_rays))
 
-    scan = np.full(num_rays, max_dist, dtype=np.float32)
-    geomgroup = None
+    scan = np.full(num_rays, max_dist, dtype=np.float32)  # initialization
+    geomgroup = None  # 모든 geom을 대상으로 함
     flg_static = 1
     bodyexclude = model.body("chassis").id
     geomid = np.array([-1], dtype=np.int32)
@@ -28,7 +28,7 @@ def get_lidar_scan(model, data, site_name, num_rays=30, max_dist=1.0):
             geomgroup, flg_static, bodyexclude, geomid
         )
         scan[i] = dist if geomid[0] != -1 else max_dist
-
+    print(scan)
     return scan
 
 
