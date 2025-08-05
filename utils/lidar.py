ec538ac081991fab1e8ec95171335d399395b4e8
Youngju <parky21@my.erau.edu>
2025-08-05 22:40:43 +0900

    Changed Lidar values
    - Lidar max distance
    - Lidar range

--- utils/lidar.py
+++ utils/lidar.py
@@ -1,16 +1,16 @@
 import numpy as np
 import mujoco
 
-def get_lidar_scan(model, data, site_name, num_rays=30, max_dist=1.0):
+def get_lidar_scan(model, data, site_name, num_rays=30, max_dist=5.0):
     site_id = model.site(site_name).id
     origin = data.site_xpos[site_id].copy()
     rot_mat = data.site_xmat[site_id].reshape(3, 3)  # Local → World 회전행렬
 
     # 차량 기준 벡터
-    forward = np.array([1, 0, 0])
-    up = np.array([0, 0, -1])  # 아래쪽
+    forward = np.array([1, 0, 0])  # theta = 0
+    up = np.array([0, 0, -1])  # theta = 90
     # 측정 각도: 
-    angles = np.radians(np.linspace(60, 120, num_rays))
+    angles = np.radians(np.linspace(30, 150, num_rays))
 
     scan = np.full(num_rays, max_dist, dtype=np.float32)
     geomgroup = None
