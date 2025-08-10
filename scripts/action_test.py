17ceb8e5fa8a7e58b492c6ee601d46354f1308ab
Youngju <parky21@my.erau.edu>
2025-08-10 21:43:13 +0900

    action값이 env에 영향 주는지 테스트
    - action_test 를 매 step마다 150 혹은 -150씩 주었을 때 return값에 차이가 있는지 테스트
    - 결과 : 두 경우에 return이 거의 일치함 ->문제

--- scripts/action_test.py
+++ scripts/action_test.py
@@ -15,7 +15,7 @@ from utils.plot import (
 def test():
     # 환경 및 모델 초기화
     env = VehicleEnv()
-    model = PPO.load("scripts/ppo_vehicle_model.zip")  # 학습된 모델 불러오기
+    #model = PPO.load("scripts/ppo_vehicle_model.zip")  # 학습된 모델 불러오기
     obs, _ = env.reset()
 
     # 로그 변수 초기화
@@ -29,32 +29,40 @@ def test():
     # LiDAR 개수
     num_rays = 32
 
-    # 실시간 플롯 초기화
-    fig, axs, lines, bars = init_all_realtime_plot(num_rays)
+    # # 실시간 플롯 초기화
+    # fig, axs, lines, bars = init_all_realtime_plot(num_rays)
 
-    # 3D MuJoCo 뷰어 실행
-    model_mjc = env.model
-    data = env.data
-    viewer = mujoco.viewer.launch_passive(model_mjc, data)
+    # # 3D MuJoCo 뷰어 실행
+    # model_mjc = env.model
+    # data = env.data
+    # viewer = mujoco.viewer.launch_passive(model_mjc, data)
 
-    # 카메라 차량 추적 설정
-    chassis_id = model_mjc.body('chassis').id
-    viewer.cam.type = mujoco.mjtCamera.mjCAMERA_TRACKING
-    viewer.cam.trackbodyid = chassis_id
+    # # 카메라 차량 추적 설정
+    # chassis_id = model_mjc.body('chassis').id
+    # viewer.cam.type = mujoco.mjtCamera.mjCAMERA_TRACKING
+    # viewer.cam.trackbodyid = chassis_id
 
     try:
         done = False
         step_count = 0
         episode_return = 0
 
-        while not done and viewer.is_running():
-            # PPO 모델로부터 액션 예측
-            action, _ = model.predict(obs, deterministic=True)
+        while not done:
+            # # PPO 모델로부터 액션 예측
+            # action, _ = model.predict(obs, deterministic=True)
+
+            # Case 1 : All +100
+            #action = [150, 150, 150, 150]
+
+            # Case 2 : All -100
+            action = [-150, -150, -150, -150]
 
             # 환경 스텝
             obs, reward, terminated, truncated, info = env.step(action)
             done = terminated or truncated
 
+            # return
+            print(reward)
             episode_return += reward
 
             # 센서 및 상태 추출
@@ -72,15 +80,15 @@ def test():
             acc_log.append(vert_acc)
             t += env.sim_dt
 
-            # 실시간 플롯 갱신
-            update_all_realtime_plot(
-                lines, bars, axs,
-                time_log, speed_log, roll_log, pitch_log, acc_log, lidar_vals
-            )
+            # # 실시간 플롯 갱신
+            # update_all_realtime_plot(
+            #     lines, bars, axs,
+            #     time_log, speed_log, roll_log, pitch_log, acc_log, lidar_vals
+            # )
 
-            # MuJoCo 화면 동기화
-            mujoco.mj_step(model_mjc, data)
-            viewer.sync()
+            # # MuJoCo 화면 동기화
+            # mujoco.mj_step(model_mjc, data)
+            # viewer.sync()
 
             step_count += 1
             if step_count > 2000:
@@ -89,15 +97,15 @@ def test():
         # 종료 처리
         print("================[Simulation Completed]================")
         print(f"Episode Return: {episode_return:.4f}")
-        save_all_plots_as_image(fig, "results/simulation_results.png")
-        close_all_realtime_plot()
-        viewer.close()
+        # save_all_plots_as_image(fig, "results/simulation_results.png")
+        # close_all_realtime_plot()
+        # viewer.close()
         env.close()
 
     except KeyboardInterrupt:
         print("Force Quit")
-        close_all_realtime_plot()
-        viewer.close()
+        # close_all_realtime_plot()
+        # viewer.close()
         env.close()
 
 if __name__ == "__main__":
